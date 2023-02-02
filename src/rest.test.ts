ddb4e5fc02c293daf3c98d0fa6e27b9bba45adca
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-03 01:36:22 +0900

    test: add test

--- src/rest.test.ts
+++ src/rest.test.ts
@@ -1,6 +1,7 @@
 import { setupServer } from "msw/node";
 import { afterAll, afterEach, beforeAll, describe, expect, test, vi } from "vitest";
 import { createRestHandler } from "./rest";
+import axios from "axios";
 
 describe("rest handler test", () => {
   const server = setupServer();
@@ -9,14 +10,16 @@ describe("rest handler test", () => {
   afterAll(() => server.close());
 
   test("define get handler", async () => {
-    const requestSpy = vi.fn();
+    const requestUrlSpy = vi.fn();
+    const requestMethodSpy = vi.fn();
     const handler = createRestHandler({
       basePath: "https://example.com/api",
       path: "/users",
       method: "get",
       statusCode: 200,
       onRequest: (req) => {
-        requestSpy(req);
+        requestUrlSpy(req.url.toString());
+        requestMethodSpy(req.method);
       },
       resolver: () => {
         return { id: 1, name: "John" };
@@ -24,10 +27,39 @@ describe("rest handler test", () => {
     });
     server.use(handler);
 
-    // expect(requestSpy).toHaveBeenCalledWith({});
-    expect(await fetch("https://example.com/api/users").then((r) => r.text())).toStrictEqual({
-      id: 1,
-      name: "John",
+    const result = await axios.get("https://example.com/api/users");
+    expect(requestUrlSpy).toHaveBeenCalledWith("https://example.com/api/users");
+    expect(requestMethodSpy).toHaveBeenCalledWith("GET");
+    expect(result.status).toBe(200);
+    expect(result.data).toStrictEqual({ id: 1, name: "John" });
+  });
+
+  test("define post handler", async () => {
+    const requestUrlSpy = vi.fn();
+    const requestBodySpy = vi.fn();
+    const requestMethodSpy = vi.fn();
+    const handler = createRestHandler({
+      basePath: "https://example.com/api",
+      path: "/users",
+      method: "post",
+      statusCode: 201,
+      onRequest: (req) => {
+        requestUrlSpy(req.url.toString());
+        requestBodySpy(req.body);
+        requestMethodSpy(req.method);
+      },
+      resolver: () => {
+        return { id: 1, name: "John" };
+      },
     });
+    server.use(handler);
+
+    const result = await axios.post("https://example.com/api/users", { name: "John" });
+
+    expect(requestUrlSpy).toHaveBeenCalledWith("https://example.com/api/users");
+    expect(requestBodySpy).toHaveBeenCalledWith({ name: "John" });
+    expect(requestMethodSpy).toHaveBeenCalledWith("POST");
+    expect(result.status).toBe(201);
+    expect(result.data).toStrictEqual({ id: 1, name: "John" });
   });
 });
