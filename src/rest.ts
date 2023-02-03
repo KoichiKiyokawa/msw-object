471c614fbe4edf69fea13372b3d5104198b09c20
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-03 19:55:17 +0900

    fix: type error

--- src/rest.ts
+++ src/rest.ts
@@ -1,28 +1,40 @@
-import { rest, RestHandler, RestRequest, ResponseResolver } from "msw";
+import {
+  rest,
+  RestHandler,
+  RestRequest,
+  ResponseResolver,
+  DefaultBodyType,
+  RestContext,
+} from "msw";
 import { joinURL } from "ufo";
 
 type MaybePromise<T> = Promise<T> | T;
 
-export type RestBuilder<ResponseData> = {
+export type RestBuilder<ResponseData, RequestBody extends DefaultBodyType> = {
   basePath: string;
   path: string;
   method: "get" | "post" | "put" | "patch" | "delete";
   statusCode: number;
   delay?: number;
-  resolver(req: RestRequest): MaybePromise<ResponseData>;
-  onRequest?: ResponseResolver;
+  resolver(req: RestRequest<RequestBody>): MaybePromise<ResponseData>;
+  onRequest?: ResponseResolver<RestRequest<RequestBody>, RestContext>;
 };
 
-export function createRestHandler<ResponseData>(builder: RestBuilder<ResponseData>): RestHandler {
-  return rest[builder.method](
-    joinURL(builder.basePath, builder.path),
-    async (req: RestRequest, res: any, ctx: any) => {
-      builder.onRequest?.(req, res, ctx);
-      return res(
-        ctx.status(builder.statusCode),
-        ctx.delay(builder.delay ?? 0),
-        ctx.json(await builder.resolver(req)),
-      );
-    },
-  );
+export function createRestHandler<ResponseData, RequestBody extends DefaultBodyType>(
+  builder: RestBuilder<ResponseData, RequestBody>,
+): RestHandler {
+  const responseResolver: ResponseResolver<RestRequest<RequestBody>, RestContext> = async (
+    req,
+    res,
+    ctx,
+  ) => {
+    builder.onRequest?.(req, res, ctx);
+    return res(
+      ctx.status(builder.statusCode),
+      ctx.delay(builder.delay ?? 0),
+      ctx.json(await builder.resolver(req)),
+    );
+  };
+
+  return rest[builder.method](joinURL(builder.basePath, builder.path), responseResolver);
 }
